690884304b7af718b0cf26513a879c436f8bd32d
Huy Nguyen <huynguyen.kh@groovetechnology.com>
2021-06-08 15:42:17 +0700

    Updating search context

--- openedx/features/courses_programs_search/views.py
+++ openedx/features/courses_programs_search/views.py
@@ -3,15 +3,15 @@ from __future__ import unicode_literals
 from django.conf import settings
 from django.views.decorators.csrf import ensure_csrf_cookie
 from util.cache import cache_if_anonymous
-import courseware.views.views
-from edxmako.shortcuts import marketing_link, render_to_response, render_to_string
+from edxmako.shortcuts import render_to_response
 from courseware.courses import (
     get_courses, 
     sort_by_start_date,
     sort_by_announcement
 )
 from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
-from openedx.core.djangoapps.catalog.utils import get_programs, get_programs_with_type
+from openedx.core.djangoapps.catalog.utils import get_programs_with_type
+from openedx.core.djangoapps.programs.models import ProgramsApiConfig
 from openedx.features.journals.api import get_journals_context
 
 
@@ -39,5 +39,7 @@ def index(request):
             'course_discovery_meanings': course_discovery_meanings,
             'programs_list': programs_list,
             'journal_info': get_journals_context(request),  # TODO: Course Listing Plugin required
+            'show_dashboard_tabs': True,
+            'show_program_listing': ProgramsApiConfig.is_enabled(),
         }
     )
